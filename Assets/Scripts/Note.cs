dd793567c33f95b96d27d42f048bc7cd2a328af5
yastri27 <yastri27@bergen.org>
2025-05-23 21:00:10 -0400

    removed a comment

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -14,19 +14,6 @@ public class Note : MonoBehaviour
 
     void Update()
     {
-        //double timeSinceInstantiated = SongManager.GetAudioSourceTime() - timeInstantiated;
-        //float t = (float)(timeSinceInstantiated / (SongManager.Instance.noteTime * 2));
-
-
-        //if (t > 1)
-        //{
-        //    Destroy(gameObject);
-        //}
-        //else
-        //{
-        //    transform.localPosition = Vector3.Lerp(Vector3.up * SongManager.Instance.noteSpawnY, Vector3.up * SongManager.Instance.noteDespawnY, t);
-        //    GetComponent<SpriteRenderer>().enabled = true;
-        //}
 
         double currentTime = SongManager.GetAudioSourceTime();
         double timeToHit =  currentTime - timeInstantiated;
