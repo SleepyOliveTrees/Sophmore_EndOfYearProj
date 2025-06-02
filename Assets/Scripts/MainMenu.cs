7410a92cba7b6e80a8ceb9c09c522a814a26b08a
SleepyOliveTrees <olismi27@bergen.org>
2025-06-02 09:42:39 -0400

    Welcome Screen + some audio and art

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void OpenSelection()
+    {
+        SceneManager.LoadScene("SongSelection");
+    }
+    public void OpenInstructions()
+    {
+        SceneManager.LoadScene("SongSelection");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
