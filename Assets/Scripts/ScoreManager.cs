1ea6b0d983948706a99c5e13ea75ee8bdcd0a510
avnkau27 <avnkau27@bergen.org>
2025-06-09 15:23:24 -0400

    End Scene Finished!

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,7 +10,9 @@ public class ScoreManager : MonoBehaviour
     public TMPro.TextMeshPro scoreText;
     public TMPro.TextMeshPro comboText;
     static int comboScore;
-    static int score;
+    private static int score;
+    private static int hitCount;
+    private static int missCount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +23,7 @@ public class ScoreManager : MonoBehaviour
     
     public static void Hit()
     {
+        hitCount++;
         score += 1 + comboScore;
         comboScore += 1;
         Instance.hitSFX.Play();
@@ -29,6 +32,7 @@ public class ScoreManager : MonoBehaviour
     {
         comboScore = 0;
         Instance.missSFX.Play();
+        missCount++;
     }
 
     // Update is called once per frame
@@ -37,4 +41,8 @@ public class ScoreManager : MonoBehaviour
         scoreText.text = score.ToString();
         comboText.text = "x" + comboScore.ToString();
     }
+
+    public static int GetScore() { return score; }
+    public static int GetHitCount() { return hitCount; }   
+    public static int GetMissCount() { return missCount; }
 }
